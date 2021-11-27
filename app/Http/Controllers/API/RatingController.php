b8f77a847b32bdf2ea4fcec9ee565f489834ab05
ryansyah17 <ryansyahputra553@gmail.com>
2021-11-27 07:31:50 +0700

    tes stek lu

--- app/Http/Controllers/API/RatingController.php
+++ app/Http/Controllers/API/RatingController.php
@@ -2,10 +2,64 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Rating;
 use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
 
 class RatingController extends Controller
 {
-    //
+    public function addRating(Request $request)
+    {
+        try {
+            $request->validate([
+                'clean_r' => ['required', 'string', 'max:255'],
+                'boardinghouses_id' => ['required', 'integer'],
+            ]);
+
+            Rating::create([
+                'clean_r' => $request->clean_r,
+                'boardinghouses_id' => $request->boardinghouses_id,
+            ]);
+
+            $Rating = Rating::where('clean_r', $request->clean_r)->first();
+
+            return ResponseFormatter::success(
+                $Rating,
+                'Data Berhasil Ditambahkan'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something Went Wrong',
+                'error' => $error,
+            ], 'Imput Data Failed', 500);
+        }
+    }
+
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+
+        if ($id) {
+            $perId = Rating::find($id);
+            if ($id) {
+                return ResponseFormatter::success(
+                    $perId,
+                    'Data Rating Berhasil Ditampilkan'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Rating Gagal Ditampilkan',
+                    404
+                );
+            }
+        }
+
+        $Rating = Rating::all();
+        return ResponseFormatter::success(
+            $Rating,
+            'Mantap'
+        );
+    }
 }
--- app/Http/Controllers/API/TypeController.php
+++ app/Http/Controllers/API/TypeController.php
@@ -7,5 +7,57 @@ use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    //
+    public function addType(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'boardinghouses_id' => ['required', 'integer'],
+            ]);
+
+            Type::create([
+                'name' => $request->name,
+                'boardinghouses_id' => $request->boardinghouses_id,
+            ]);
+
+            $Type = Type::where('name', $request->name)->first();
+
+            return ResponseFormatter::success(
+                $Type,
+                'Data Berhasil Ditambahkan'
+            );
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something Went Wrong',
+                'error' => $error,
+            ], 'Imput Data Failed', 500);
+        }
+    }
+
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+
+        if ($id) {
+            $perId = Type::find($id);
+            if ($id) {
+                return ResponseFormatter::success(
+                    $perId,
+                    'Data Type Berhasil Ditampilkan'
+                );
+            } else {
+                return ResponseFormatter::error(
+                    null,
+                    'Data Type Gagal Ditampilkan',
+                    404
+                );
+            }
+        }
+
+        $Type = Type::all();
+        return ResponseFormatter::success(
+            $Type,
+            'Mantap'
+        );
+    }
 }
